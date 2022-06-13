377f4fac227fd378988a3d0c961fc7f588d99d3e
mon-neutrino <monica.sing.2023@gmail.com>
2022-06-13 21:13:51 +0700

    Initial Commit

---
+++ idk anymore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  idk anymore
+//
+//  Created by Monica Sing on 13/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
