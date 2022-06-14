159974d6cfadda07488c7db42cec0f96f46b0ab8
mon-neutrino <monica.sing.2023@gmail.com>
2022-06-15 00:29:57 +0700

    test

--- idk anymore/ViewController.swift
+++ idk anymore/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var thisMonica: UILabel!
+    
+    @IBOutlet weak var whaButton: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        thisMonica.isHidden = true
+        whaButton.isHidden = true
     }
 
-
+    @IBAction func showMonica(_ sender: UIButton) {
+        thisMonica.isHidden = false
+        whaButton.isHidden = false
+    }
+    
 }
 
